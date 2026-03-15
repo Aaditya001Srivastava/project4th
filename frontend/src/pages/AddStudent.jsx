32507105821248f496cbe96b8e10fd25b78a9032
Aaditya laptop <aaditya.srivastava0401@gmail.com>
2026-03-15 17:44:21 +0530

    fix webcam import crash

--- frontend/src/pages/AddStudent.jsx
+++ frontend/src/pages/AddStudent.jsx
@@ -1,8 +1,8 @@
 // Filename: AddStudent.jsx
 
 import React, { useRef, useState } from "react";
-//import Webcam from "react-webcam";
-const Webcam = require("react-webcam").default;
+import Webcam from "react-webcam";
+//const Webcam = require("react-webcam").default;
 console.log("THIS ADDSTUDENT IS LOADED");
 
 export default function AddStudent() { 
