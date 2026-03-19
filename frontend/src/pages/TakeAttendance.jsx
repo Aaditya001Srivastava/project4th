d85ceccc40a0b375c93b5886997aec056c42b227
Aaditya laptop <aaditya.srivastava0401@gmail.com>
2026-03-19 14:41:56 +0530

    fixed name variable

--- frontend/src/pages/TakeAttendance.jsx
+++ frontend/src/pages/TakeAttendance.jsx
@@ -99,7 +99,7 @@ console.log("Distance from IERT (km):", distance);
 
 // ✅ FIXED LOGIC
 if (distance > RADIUS) {
-  alert((name||"Student")+" - You are not inside IERT campus!");
+  alert(("Student")+" - You are not inside IERT campus!");
   return;
 }
 
